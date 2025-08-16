a8f2fc3baa538bcf5da00366ab64922b1b3306e9
jaeju kim <jaeju89.kim@samsung.com>
2025-08-16 13:50:57 +0900

    env: aarch64: update environment script

--- env.aarch64.sh
+++ env.aarch64.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 # Helper to set PATH for aarch64-elf toolchain and provide a QEMU runner.
 
-set -euo pipefail
-
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
 # Prefer the x86_64 toolchain (works via Rosetta on Apple Silicon).
